7c30a96d562d7999bd6d3c34a3b4b6db6e0c24aa
JANGSAMMY <toal7822@gmail.com>
2025-09-15 18:14:38 +0900

    새미 수정

--- src/component/Magazine/magazine1/Magazine1.jsx
+++ src/component/Magazine/magazine1/Magazine1.jsx
@@ -14,8 +14,10 @@ const Magazine1 = () => {
                         <li>요즘 히트곡들은 모두 Vince의 손끝에서 만들어진다. </li>
                         <li>
                             <img src="./images/main/main_con6_04.jpg" alt="로제" />
-                            <h3>로제&브루노 마스</h3>
-                            <span>댓글 96 | 조회 1,872</span>
+                            <div className="mg-title">
+                                <h3>로제&브루노 마스</h3>
+                                <span>댓글 96 | 조회 1,872</span>
+                            </div>
                             <p>
                                 가수 로제와 브루노 마스가 함께 불렀으며, 한국의 술자리 게임
                                 '아파트'에서 영감을 얻어 만들어졌습니다. 
@@ -32,8 +34,11 @@ const Magazine1 = () => {
                                 alt="임영웅"
                                 style={{ width: 340 }}
                             />
-                            <h3>임영웅</h3>
-                            <span>댓글 96 | 조회 1,872</span>
+                            <div className="mg-title">
+                                <h3>임영웅</h3>
+                                <span>댓글 96 | 조회 1,872</span>
+                            </div>
+
                             <p>
                                 '순간을 영원처럼'은 임영웅의 따뜻한 보컬과 평범한 일상 속에서 서로
                                 미워하지 말고 사랑하며 순간을 영원처럼 살아가자는 메시지가 있습니다
@@ -41,8 +46,10 @@ const Magazine1 = () => {
                         </li>
                         <li>
                             <img src="./images/magazine/magazine02.jpg" alt="제니" />
-                            <h3>제니</h3>
-                            <span>댓글 96 | 조회 1,872</span>
+                            <div className="mg-title">
+                                <h3>제니</h3>
+                                <span>댓글 96 | 조회 1,872</span>
+                            </div>
                             <p>
                                 멋진 태도와 퍼포먼스에 반한 많은 사람들이 'like JENNIE'의 댄스를
                                 따라 추며 호응을 보내고 있습니다.
@@ -64,8 +71,10 @@ const Magazine1 = () => {
                                 alt="코르티스"
                                 style={{ width: 400 }}
                             />
+
                             <h3>선 밖에서 나타난 코르티스, 자신들을 보여준 패기의 '영크크'</h3>
                             <span>댓글 96 | 조회 1,872</span>
+
                             <p>
                                 'What You Want'는 새로운 세상에 발을 들인 코르티스의 다짐을 담은
                                 선언문이다. 1960년대 사이키델릭 록의 향수를 담은 기타 리프와 힙합
@@ -84,8 +93,10 @@ const Magazine1 = () => {
                     <ul className="magazine24">
                         <li>
                             <img src="./images/magazine/magazine05.jpg" alt="아이브" />
-                            <h3> 아이브(IVE)</h3>
-                            <span>댓글 96 | 조회 1,872</span>
+                            <div className="mg-title">
+                                <h3> 아이브(IVE)</h3>
+                                <span>댓글 96 | 조회 1,872</span>
+                            </div>
                             <p>
                                 완벽함의 틈을 인정하면서도 그것마저 하나의 매력으로 전환하는 방식.
                                 이번 앨범은 그렇게 아이브다운 비밀을 만들었습니다.
@@ -94,8 +105,10 @@ const Magazine1 = () => {
 
                         <li>
                             <img src="./images/magazine/magazine06.jpg" alt="전소미" />
-                            <h3> 전소미</h3>
-                            <span>댓글 96 | 조회 1,872</span>
+                            <div className="mg-title">
+                                <h3> 전소미</h3>
+                                <span>댓글 96 | 조회 1,872</span>
+                            </div>
                             <p>더블랙레이블 주가 더 끌어올린 터줏대감의 품격.</p>
                         </li>
                     </ul>
